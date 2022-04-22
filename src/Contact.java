8b976507b9757f0d4765afa40d20b5f5b0f5a051
Richard Jackson <rpjackson2@gmail.com>
2022-04-22 15:03:52 -0500

    added method

--- src/Contact.java
+++ src/Contact.java
@@ -1,7 +1,10 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Contact {
     static String name;
     static String phoneNumber;
-
+    private static final Scanner scanner = new Scanner(System.in);
     public Contact(String name, String phoneNumber ){
         Contact.name = name;
         Contact.phoneNumber = phoneNumber;
@@ -10,4 +13,20 @@ public class Contact {
     public static void showContacts() {
         
     }
+
+    //    TODO: Add number to memory
+    public static void addContact(String newName,String phoneNumber){
+        HashMap<String,String> newPerson = new HashMap<>();
+        newPerson.put(newName,phoneNumber);
+        System.out.println(newPerson);
+    }
+    public static void getContact(){
+        System.out.println("enter new contact...");
+        System.out.println("Enter first and last name.");
+        String nameInput = scanner.nextLine();
+        System.out.println("Enter phone number.");
+        String phNumberInput = scanner.nextLine();
+        addContact(nameInput,phNumberInput);
+    }
+
 }
