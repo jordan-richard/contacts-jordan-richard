e1947e3e7a3eb13ec4092048d7b23ff98d1983c4
Richard Jackson <rpjackson2@gmail.com>
2022-04-22 19:20:28 -0500

    added arraylist functionality

--- src/Contact.java
+++ src/Contact.java
@@ -1,32 +1,88 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.io.*;
 
 public class Contact {
-    static String name;
-    static String phoneNumber;
-    private static final Scanner scanner = new Scanner(System.in);
-    public Contact(String name, String phoneNumber ){
-        Contact.name = name;
-        Contact.phoneNumber = phoneNumber;
+    String name;
+    String phoneNumber;
+    static HashMap<String, String> newPerson = new HashMap<>();
+//    static ArrayList<HashMap> newPersonList = new ArrayList<>();
+    public final Scanner scanner = new Scanner(System.in);
+
+    //   TODO: Constructor function
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
     }
 
-    public static void showContacts() {
-        
+    //    TODO: Getters and Setters
+    public String getName() {
+        return name;
     }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    static ArrayList<Contact> contactList = new ArrayList<Contact>();
+    static Scanner scan = new Scanner(System.in);
+    static boolean isRunning = true;
+
+    public static void runApp(){
+        while(isRunning){
+            System.out.println("Please enter contact name or (quit to exit program): ");
+            String name = scan.next();
+            if(name.equalsIgnoreCase("quit")) {
+                System.out.println("Thank you. GoodBye!");
+                break;
+            }
+            System.out.println("Please enter phone number: ");
+            String phoneNumber = scan.next();
+            Contact newContact = new Contact(name, phoneNumber);
+            contactList.add(newContact);
+    }
+        for(Contact t: contactList) {
+            System.out.println("Name: " + t.getName() + "\nAmount: " + t.getPhoneNumber());
+            System.out.println();
+        }
+}
+
+
+//    public static void showContacts() {
+//
+//    }
 
     //    TODO: Add number to memory
-    public static void addContact(String newName,String phoneNumber){
-        HashMap<String,String> newPerson = new HashMap<>();
-        newPerson.put(newName,phoneNumber);
-        System.out.println(newPerson);
-    }
-    public static void getContact(){
-        System.out.println("enter new contact...");
-        System.out.println("Enter first and last name.");
-        String nameInput = scanner.nextLine();
-        System.out.println("Enter phone number.");
-        String phNumberInput = scanner.nextLine();
-        addContact(nameInput,phNumberInput);
-    }
+//    public static void addContact(String newName,String phoneNumber){
+////        HashMap<String,String> newPerson = new HashMap<>();
+//        newPerson.put(newName,phoneNumber);
+////        newPerson.put("fff","phoneNumber");
+////        newPersonList.add(newPerson);
+//        System.out.println(newPerson);
+////        System.out.println(newPerson.get("richard jackson"));
+////        for(int i=0;i < newPerson.size();i++){
+////            System.out.println(newPerson.get(i));
+////        }
+//    }
+//    public static void getContact(){
+//        System.out.println("enter new contact...");
+//        System.out.println("Enter first and last name.");
+//        String nameInput = scanner.nextLine();
+//        System.out.println("Enter phone number.");
+//        String phNumberInput = scanner.nextLine();
+//        addContact(nameInput,phNumberInput);
+//    }
+
+//public static void writeFile(String fileName, HashMap<String,String> personData ){
+//try{
+//    FileWriter fw = new FileWriter();
+//}
+//}
+//catch( IOException ex){
 
 }
+
+
+
+
--- src/ContactManager.java
+++ src/ContactManager.java
@@ -3,22 +3,23 @@ import java.util.Scanner;
 public class ContactManager {
 
     public static void main(String[] args) {
+//        Input input = new Input();
+//        mainMenu();
 
-        Input input = new Input();
-        mainMenu();
-
+Contact.runApp();
     }
 
-    private static void mainMenu() {
-        System.out.println("What would you like to do?");
-        System.out.println("1. Show All Contacts");
-        System.out.println("2. Add New Contact");
-        Scanner s = new Scanner(System.in);
-        String answer = s.nextLine();
-
-        if (answer.equals("2")) {
-            Input.addContact(Contact.name, Contact.phoneNumber);
-            mainMenu();
-        }
+//   public static void mainMenu() {
+//        System.out.println("What would you like to do?");
+//        System.out.println("1. Show All Contacts");
+//        System.out.println("2. Add New Contact");
+//        Scanner s = new Scanner(System.in);
+//        String answer = s.nextLine();
+//
+//        if (answer.equals("2")) {
+//
+////            Input.addContact(Contact.name, Contact.phoneNumber);
+//            mainMenu();
+//        }
     }
-}
+
